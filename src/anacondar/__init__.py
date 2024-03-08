f38c66f7822cc66439cdd2e1841b6702aaf21bf6
Max Parry <66670928+Buggem@users.noreply.github.com>
2024-03-09 10:39:19 +1100

    Add exit command support

--- src/anacondar/__init__.py
+++ src/anacondar/__init__.py
@@ -3,15 +3,21 @@ from subprocess import run
 import socket
 import os
 import sys
-
+running = False
 def cd(direc):
   os.chdir(direc)
+def exit():
+  running = False
 def terminal():
+  running = True
   print(f'Anacondar - version 0.1 on {sys.platform.title()}')
   print(f'')
-  while True:
+  while running:
     cmd = input(f'{socket.gethostname()}:{os.getcwd()} $ ')
     if cmd.split(' ')[0] == 'cd':
       cd(cmd.split(' ')[len(cmd.split(' '))-1])
+      continue
+    if cmd.split(' ')[0] == 'exit':
+      exit()
       continue
     run(cmd, shell=True)
